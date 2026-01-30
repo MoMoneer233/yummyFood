f5e7ceed726015eaf5725a5a7db235d4a0ea8bce
MoMoneer233 <monneer233@gmail.com>
2026-01-31 01:32:32 +0300

    Add categories section and scroll-to-top button
    Introduced a new categories section with images in index.html and corresponding styles in style.css. Added a scroll-to-top button with display logic and smooth scrolling behavior in scripts.js. Improved header and navigation styles, and updated the search input placeholder.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
 menu.addEventListener("click", () => {
   mobileNav.classList.toggle("showMobileNav");
 });
+
+let topBtn = document.getElementById("top");
+this.addEventListener("scroll", () => {
+  if (scrollY >= 600) {
+    topBtn.style.display = "flex";
+  } else {
+    topBtn.style.display = "none";
+  }
+});
+
+topBtn.addEventListener("click", () => {
+  scrollTo({
+    top:0,
+    left:0,
+    behavior:"smooth"
+  });
+});
